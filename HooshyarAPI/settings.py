48e0997fdb4bb22d4c634ab77ab20df859c0fdfb
miladallahgholi95 <miladallahgholi95@gmail.com>
2024-07-30 14:56:38 +0330

    change index name

--- HooshyarAPI/settings.py
+++ HooshyarAPI/settings.py
@@ -138,13 +138,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
-MEDIA_URL = '/media/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
