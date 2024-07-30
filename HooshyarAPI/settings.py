3c45e13dd0e1bafedbe2ef9374a2ac67db4b9254
miladallahgholi95 <miladallahgholi95@gmail.com>
2024-07-30 15:04:59 +0330

    change index name

--- HooshyarAPI/settings.py
+++ HooshyarAPI/settings.py
@@ -58,13 +58,11 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SECURE_SSL_HOST = "virtualjuristic.datakaveh.com"
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
+# SECURE_SSL_HOST = "virtualjuristic.datakaveh.com"
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-
 
 ROOT_URLCONF = 'HooshyarAPI.urls'
 
